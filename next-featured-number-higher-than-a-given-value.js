58bb2a067141d9c3972c977e105bc3cfcbc3e84a
Kyle Smelter <kylesmelter@gmail.com>
2022-12-17 18:10:44 -0800

    next-featured-number-higher-than-a-given-value

---
+++ next-featured-number-higher-than-a-given-value.js
@@ -0,0 +1,28 @@
+// Make a function that receives a value, val and outputs the smallest higher number than the given value, and this number belong to a set of positive integers that have the following properties:
+
+// their digits occur only once
+
+// they are odd
+
+// they are multiple of three
+
+// nextNumb(12) == 15
+
+// nextNumb(13) == 15
+
+// nextNumb(99) == 105
+
+// nextNumb(999999) == 1023459
+
+// nextNumber(9999999999) == "There is no possible number that
+// fulfills those requirements"
+// Enjoy the kata!!
+
+function nextNumb(val) {
+    for (let i = val + 1; i < 9999999999; i++) {
+      if (new Set(String(i).split('')).size === String(i).split('').length && i % 2 !== 0 && i % 3 === 0) {
+        return i;
+      };
+    };
+    return 'There is no possible number that fulfills those requirements';
+};
