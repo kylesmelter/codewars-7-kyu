f0204830881be0e48f75712506e6fccc50162a6a
Kyle Smelter <kylesmelter@gmail.com>
2023-04-25 20:28:17 -0700

    sort-array-1

---
+++ sort-array-1.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    // All we need to do here is return the .sort() method without any callbacks 
+    return names.sort();
+};
