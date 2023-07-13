663d2679660464db8e4af2508d4e3d14b99cd0ea
Kyle Smelter <kylesmelter@gmail.com>
2023-07-12 20:31:51 -0700

    likes-vs-dislikes

--- likes-vs-dislikes.js
+++ likes-vs-dislikes.js
@@ -1,10 +1,10 @@
-// Story
-// YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+// YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up 
+// in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about 
+// the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the 
+// dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
 
-// Task
 // Create a function that takes in a list of button inputs and returns the final state.
 
-// Examples
 // likeOrDislike([Dislike]) => Dislike
 // likeOrDislike([Like,Like]) => Nothing
 // likeOrDislike([Dislike,Like]) => Like
@@ -14,18 +14,19 @@
 // If the list is empty, return Nothing.
 
 function likeOrDislike(buttons) {
-    // We'll start by creating a variable, 'result,' and initially set it to
-    // 'Nothing' in case the input array is empty
-    let result = 'Nothing';
-    // Next we'll create a for-loop so we can iterate over the input array, 'buttons'
-    for (let i = 0; i < buttons.length; i++) {
-    // Inside the for-loop we'll have an if-conditional to check the current value
-    // and compare it to the value of our 'result' variable. The value of 'result'
-    // will become whatever the current value is
-    if (buttons[i] === result) {
-      result = 'Nothing';
-    } else {
+  // We'll first check if the length of the input array, 'buttons,' === 0, and if it does we'll return 'Nothing'
+  if(buttons.length === 0) {
+    return 'Nothing';
+  };
+  // Next, we'll create a variable, 'result,' with no inital value as it will be assigned later during a for-loop
+  let result;
+  // Now we'll create that for-loop we just mentioned
+  for(let i = 0; i <= buttons.length - 1; i++) {
+    // Then we'll have an if-conditional that will check what the current value is, and assign a value to 'result' based off it
+    if(buttons[i] !== result) {
       result = buttons[i];
+    } else if(buttons[i] === result) {
+      result = 'Nothing';
     };
   };
   // All we need to do now is return 'result!'
