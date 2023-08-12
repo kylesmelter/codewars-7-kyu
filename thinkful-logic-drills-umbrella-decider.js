cb834160e448b0ec8079183b4b6f8981e1a26a18
Kyle Smelter <kylesmelter@gmail.com>
2023-08-12 12:46:42 -0700

    thinkful-logic-drills-umbrella-decider

---
+++ thinkful-logic-drills-umbrella-decider.js
@@ -0,0 +1,21 @@
+// Write a function take_umbrella() that takes two arguments: a string representing the current weather and a float representing the chance of rain today.
+
+// Your function should return True or False based on the following criteria.
+
+// You should take an umbrella if it's currently raining or if it's cloudy and the chance of rain is over 0.20.
+// You shouldn't take an umbrella if it's sunny unless it's more likely to rain than not.
+// The options for the current weather are sunny, cloudy, and rainy.
+
+// For example, take_umbrella('sunny', 0.40) should return False.
+
+// As an additional challenge, consider solving this kata using only logical operaters and not using any if statements.
+
+function takeUmbrella(weather, chance) {
+    // We'll use an if-conditional to make sure 'true' is returned in all the
+    // correct scenarios, and false if not
+    if(weather === 'rainy' || weather === 'cloudy' && chance > 0.20 || weather === 'sunny' && chance > 0.50) {
+      return true;
+    } else {
+      return false;
+    };
+};
